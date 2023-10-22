bea283cc2f652e37d69e500be2f750c032fa3b9b
nandishnagaraj <47733024+nandishnagaraj@users.noreply.github.com>
2023-10-22 21:02:40 +0530

    logo no background

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -143,9 +143,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, "greatkart/static"),
+    os.path.join(BASE_DIR, 'greatkart/static'),  # Add any additional directories as needed
 ]
 
 # Default primary key field type
