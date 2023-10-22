7bc40ebbaaaba7ed5919c94036148df99c21614d
nandishnagaraj <47733024+nandishnagaraj@users.noreply.github.com>
2023-10-22 14:24:27 +0530

    checking

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,4 +1,4 @@
-import logging
+
 from django.shortcuts import render
 from store.models import Product
 import os
@@ -6,11 +6,8 @@ import glob
 
 def home(request):
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    logging.info("====>",base_dir)
     image_folder = os.path.join(base_dir, 'greatkart\static\images\\banners')
-    logging.info("====>",image_folder)    
     image_paths = glob.glob(os.path.join(image_folder, "*.jpg"))
-    logging.info("====>",image_paths)  # Adjust the file extension as needed
     
     products = Product.objects.all().filter(is_available=True).order_by('created_date')
     context = {
