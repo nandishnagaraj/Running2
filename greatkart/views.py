c8219ba02801a2f9e07ba576855ca1dafa2b71f9
nandishnagaraj <47733024+nandishnagaraj@users.noreply.github.com>
2023-10-22 13:56:02 +0530

    print

--- greatkart/views.py
+++ greatkart/views.py
@@ -5,11 +5,11 @@ import glob
 
 def home(request):
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    print(base_dir)
+    print("====>",base_dir)
     image_folder = os.path.join(base_dir, 'greatkart\static\images\\banners')
-    print(image_folder)    
+    print("====>",image_folder)    
     image_paths = glob.glob(os.path.join(image_folder, "*.jpg"))
-    print(image_paths)  # Adjust the file extension as needed
+    print("====>",image_paths)  # Adjust the file extension as needed
     
     products = Product.objects.all().filter(is_available=True).order_by('created_date')
     context = {
