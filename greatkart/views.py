576f1dcca0c966d05197babc199cefa28d575307
nandishnagaraj <47733024+nandishnagaraj@users.noreply.github.com>
2023-10-22 11:16:02 +0530

    carousel

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from store.models import Product
+import os
+import glob
 
 def home(request):
+    image_folder = "./static/images/banners"
+    image_paths = glob.glob(os.path.join(image_folder, "*.jpg"))  # Adjust the file extension as needed
+    
     products = Product.objects.all().filter(is_available=True).order_by('created_date')
     context = {
         'products': products,
+        'image_paths': image_paths,
     }
     return render(request, 'home.html', context)
