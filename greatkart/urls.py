85cba364662e35f0e07e903962fb8d2bcc54dba0
nandishnagaraj <47733024+nandishnagaraj@users.noreply.github.com>
2023-11-01 16:33:13 +0530

    Terms and condition

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
     path('privacy-policy/', TemplateView.as_view(template_name='privacy_policy.html'), name='privacy_policy'),
+    path('termsofuse/', TemplateView.as_view(template_name='termsofuse.html'), name='termsofuse'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
